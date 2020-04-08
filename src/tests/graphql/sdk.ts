393424f255cb391953edf61b632ca2016838475a
Chris Langager <christopher.langager@gmail.com>
2020-04-08 11:35:10 -0600

    import instead of require

--- src/tests/graphql/sdk.ts
+++ src/tests/graphql/sdk.ts
@@ -2,8 +2,9 @@ import { GraphQLClient } from 'graphql-request';
 import { getSdk } from './generated/sdk';
 
 //this nonsense is to get cookies to work in our tests
-const IsomorphicFetch = require('real-isomorphic-fetch');
-const fetch = require('node-fetch');
+import * as IsomorphicFetch from 'real-isomorphic-fetch';
+import * as fetch from 'node-fetch';
+
 global['fetch'] = new IsomorphicFetch(fetch);
 
 const url = 'http://localhost:5000/graphql';
@@ -13,7 +14,6 @@ export type Sdk = ReturnType<typeof getSdk>;
 export function newSdk() {
   const client = new GraphQLClient(url, {
     credentials: 'include',
-    mode: 'cors',
   });
   return getSdk(client);
 }
