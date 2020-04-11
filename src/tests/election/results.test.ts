b74f1ec08993bee70285c0ec9e354dac29f57e0a
Chris Langager <christopher.langager@gmail.com>
2020-04-11 13:52:11 -0600

    code is rough, but results are being calculated when an election is stopped

--- src/tests/election/results.test.ts
+++ src/tests/election/results.test.ts
@@ -4,7 +4,6 @@ import { createStartedElection } from './util';
 describe('results', () => {
   it('results should be null until an election is closed', async () => {
     const { id } = await createStartedElection();
-
     const response = await sdk.getElection({ id });
     expect(response.election.results).toBeNull();
   });
@@ -12,7 +11,6 @@ describe('results', () => {
   it('a closed election with no ballots cast should not have results', async () => {
     const { id } = await createStartedElection();
     await sdk.stopElection({ input: { electionId: id } });
-
     const response = await sdk.getElection({ id });
     expect(response.election.results).toBeNull();
   });
@@ -32,5 +30,55 @@ describe('results', () => {
 
     const response = await sdk.getElection({ id });
     expect(response.election.results).not.toBeNull();
+  });
+
+  it.only('multiple rounds', async () => {
+    const { id, candidates } = await createStartedElection();
+    const candidateIds = candidates.map(({ id }) => id);
+    const A = candidateIds[0];
+    const B = candidateIds[1];
+    const C = candidateIds[2];
+
+    await sdk.castBallot({
+      input: {
+        electionId: id,
+        candidateIds: [A, C],
+      },
+    });
+
+    await sdk.castBallot({
+      input: {
+        electionId: id,
+        candidateIds: [A, C],
+      },
+    });
+
+    await sdk.castBallot({
+      input: {
+        electionId: id,
+        candidateIds: [B, C],
+      },
+    });
+
+    await sdk.castBallot({
+      input: {
+        electionId: id,
+        candidateIds: [C],
+      },
+    });
+    await sdk.castBallot({
+      input: {
+        electionId: id,
+        candidateIds: [C],
+      },
+    });
+
+    await sdk.stopElection({ input: { electionId: id } });
+
+    const response = await sdk.getElection({ id });
+    expect(response.election.results).not.toBeNull();
+
+    expect(response.election.results.winner.id).toEqual(C);
+    expect(response.election.results.rounds.length).toEqual(2);
   });
 });
